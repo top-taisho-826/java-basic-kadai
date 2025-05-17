151ea217dc6890b88e1595f0fa2aa38adc1d0371
top-taisho-826 <top.taisho.826@gmail.com>
2025-05-17 18:10:39 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,9 +1,8 @@
 package kadai_015;
 
 public class CarExec_Chapter15 {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
 
 	}
 
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,22 +1,30 @@
 package kadai_015;
 
 public class Car_Chapter15 {
-//フィールド
+	//フィールド
 	private int gear = 1;
 	private int speed = 10;
 	
+	//コンストラクタ
+	public Car_Chapter15(int gear, int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+	
 	//メソッド
 	public void gearChange(int afterGear) {
-		swich(this.gear){
-			case 1 -> 10;
-			case 2 -> 20;
-			case 3 -> 30;
-			case 4 -> 40;
-			case 5 -> 50;
-			default -> 10;
-		}
-	}
-	public void run() {
-		
+		System.out.println("ギア" + this.gear + "から" + "afterGear" + "に切り替えました");
+	
+	
+	int speed = switch(afterGear){
+		case 1 -> this.speed * 1;
+		case 2 -> this.speed * 2;
+		case 3 -> this.speed * 3;
+		case 4 -> this.speed * 4;
+		case 5 -> this.speed * 5;
+		default -> 10;
+	};
+
+		System.out.println("速度は時速" + speed + "kmです" );		
 	}
 }
