f67f6803d9d184af69aba5720d365be21b471d52
top-taisho-826 <top.taisho.826@gmail.com>
2025-05-21 07:53:58 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,9 +1,11 @@
 package kadai_015;
 
 public class CarExec_Chapter15 {
-	public static void main(String[] args) {
-		
-
+	    public static void main(String[] args) {
+	        Car_Chapter15 car = new Car_Chapter15();
+	        car.gearChange(1); // ギア1
+	        car.gearChange(3); // ギア3
+	        car.gearChange(6); // ギア範囲外（時速10kmに設定される）
+	        car.run();
+	    }
 	}
-
-}
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,30 +1,41 @@
 package kadai_015;
 
+//車クラス
 public class Car_Chapter15 {
-	//フィールド
-	private int gear = 1;
-	private int speed = 10;
-	
-	//コンストラクタ
-	public Car_Chapter15(int gear, int speed) {
-		this.gear = gear;
-		this.speed = speed;
-	}
-	
-	//メソッド
-	public void gearChange(int afterGear) {
-		System.out.println("ギア" + this.gear + "から" + "afterGear" + "に切り替えました");
-	
-	
-	int speed = switch(afterGear){
-		case 1 -> this.speed * 1;
-		case 2 -> this.speed * 2;
-		case 3 -> this.speed * 3;
-		case 4 -> this.speed * 4;
-		case 5 -> this.speed * 5;
-		default -> 10;
-	};
+ // フィールド（内部データ）
+ private int gear =  1;    // 1速から5速のギアを表す
+ private int speed = 10;    // ギアチェンジ後の速度を表す    
 
-		System.out.println("速度は時速" + speed + "kmです" );		
-	}
+ // コンストラクタ（初期化処理）
+ public Car_Chapter15(int gear, int speed) {
+     this.gear = gear;
+     this.speed = speed;
+ }
+
+ public int getGear() {
+     return this.gear;
+ }
+
+ public int getSpeed() {
+     return this.speed;
+ }
+
+ // メソッド定義
+ public void gearChange(int afterGear) {
+     this.gear = afterGear;
+     // ギアの値により速度を変える
+     this.speed = switch (this.gear) {
+         case 1 -> 10;
+         case 2 -> 20;
+         case 3 -> 30;
+         case 4 -> 40;
+         case 5 -> 50;
+         default -> 10;
+     };
+ }
+
+ public void run() {
+     System.out.println("ギア" + this.getGear() + "から" + this.getGear() + "に切り替えました");
+     System.out.println("速度は時速" + this.getSpeed() + "kmです");
+ }
 }
